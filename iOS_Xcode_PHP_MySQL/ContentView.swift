65cf341a5791d5561450924102a7380a333a307d
Iclementem <iclementem@outlook.es>
2022-09-13 11:47:47 +0200

    Initial Commit

---
+++ iOS_Xcode_PHP_MySQL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Xcode_PHP_MySQL
+//
+//  Created by Iván Clemente Moreno on 13/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_Xcode_PHP_MySQL/iOS_Xcode_PHP_MySQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Xcode_PHP_MySQLApp.swift
+//  iOS_Xcode_PHP_MySQL
+//
+//  Created by Iván Clemente Moreno on 13/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Xcode_PHP_MySQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
